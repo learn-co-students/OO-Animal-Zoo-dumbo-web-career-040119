112518a9f2ab6e121e46a57a0cc0df9addcf3f3d
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-08 18:15:34 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,30 @@
 class Animal
 
+  attr_accessor :weight
+  attr_reader :species, :nickname, :zoo
+
+  ALL = []
+
+  def initialize(species, weight, nickname, zoo)
+    @species = species
+    @weight = weight
+    @nickname = nickname
+    @zoo = zoo
+    ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+
+  def self.find_by_species(animal_species)
+      self.all.select do |animal|
+      animal.species == animal_species
+    end
+  end
+
+  def self.find_by_species(animal_species)
+      ALL.select { |animal| animal.species == animal_species }
+  end
+
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,64 @@
 class Zoo
 
+  attr_reader :name, :location, :animals
+
+  ALL = []
+
+  def initialize(name, location)
+    @name = name
+    @location = location
+    ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+
+  # def animals
+  #   Animal.all
+  # end
+
+  def animals
+    Animal.all.select do |animal|
+      animal.zoo == self
+    end
+  end
+
+  # def animal_species
+  #   Animal.all.map do |animal|
+  #     animal.species
+  #   end.uniq
+  # end
+  #
+  def animal_species
+    self.animals.map do |animal|
+      animal.species
+    end.uniq
+  end
+
+  def animal_species
+    self.animals.map(&:species).uniq
+  end
+
+  def find_by_species(animal_species)
+    self.animals.select do |animal|
+      animal_species == animal.species
+    end
+  end
+
+  def animal_nicknames
+    self.animals.map do |animal|
+      animal.nickname
+    end
+
+    # self.animals.map(&:nickname)
+  end
+
+  def self.find_by_location(location)
+    self.all.select do |zoo|
+      zoo.location == location
+    end
+  end
+
+
 end
