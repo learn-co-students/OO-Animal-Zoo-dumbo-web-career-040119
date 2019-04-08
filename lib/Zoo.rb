5da907b0a21ede7b989ccd02ee495545125e0f22
github username <github email address>
2019-04-08 18:01:40 -0400

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,38 @@
 class Zoo
 
+  @@all = []
+  attr_reader :title, :location
+  attr_accessor :animal
+
+  def initialize(title, location)
+    @title = title
+    @location = location
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_location(location)
+    @@all.select {|zoo| zoo.location == location}
+  end
+
+  def animals
+    Animal.all.select {|animal| animal.zoo == self}
+  end
+
+  def animal_species
+    species_array = self.animals.select.uniq {|animal| animal.species}
+    species_array.map {|animal| animal.species}
+  end
+
+  def find_by_species(species)
+    self.animals.select {|animal| animal.species == species}
+  end
+
+  def animal_nicknames
+    self.animals.map {|animal| animal.nickname}
+  end
+
 end
--- run.rb
+++ run.rb
@@ -3,8 +3,23 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+aswp = Zoo.new("African Safari Wildlife Park", "OH")
+co = Zoo.new("CuriOdyssey", "CA")
 
+monkey = Animal.new("Monkey", 40, "Little Devil")
+donkey = Animal.new("Donkey", 100, "Eeyore")
+donkey2 = Animal.new("Donkey", 500, "Chonker")
+donkey3 = Animal.new("Donkey", 140, "Mike")
+donkey4 = Animal.new("Donkey", 120, "Mary")
+giraffe = Animal.new("Giraffe", 220, "Long neck dude")
+
+monkey.zoo = co
+donkey.zoo = co
+donkey2.zoo = co
+donkey3.zoo = aswp
+donkey4.zoo = aswp
+giraffe.zoo = co
 
 binding.pry
+
 puts "done"
